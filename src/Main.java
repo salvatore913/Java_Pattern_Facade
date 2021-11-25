93b4cbfc6274f604ff9bb7ba02871c39a865321e
Ivanova Irina <chuda913@ya.ru>
2021-11-25 20:27:30 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("101", "111"));
+        System.out.println(bins.mult("101", "111"));
+    }
+}
